1c7bdc054bcc45a976cc2c67bbd9fe36d831fbb9
Tomas Kutnius <tomas.kutniauskis@gmail.com>
2025-02-20 08:52:50 +0100

    finished create screen and started edit screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context as BlogPostContext } from '../context/BlogContext';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(BlogPostContext);
@@ -12,8 +13,23 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Edit', { id: navigation.getParam('id') })
+        }
+      >
+        <FontAwesome name='pencil' size={30} color='black' />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
